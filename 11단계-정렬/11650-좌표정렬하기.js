b09fd63a750763019536ecf66ad7021100a7f55a
dldnjswns31 <dldnjswns32@gmail.com>
2023-01-09 20:15:11 +0900

    11650-좌표정렬

---
+++ 11단계-정렬/11650-좌표정렬하기.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "../ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const num = input.shift();
+
+console.log(
+  input
+    .sort((a, b) => {
+      const aSplit = a.split(" ");
+      const bSplit = b.split(" ");
+      return aSplit[1] - bSplit[1];
+    })
+    .sort((a, b) => {
+      const aSplit = a.split(" ");
+      const bSplit = b.split(" ");
+      return aSplit[0] - bSplit[0];
+    })
+    .join("\n")
+);
