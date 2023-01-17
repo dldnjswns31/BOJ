e35c5d36e9fde24c8b511e3c318856e8e4493e72
dldnjswns31 <dldnjswns32@gmail.com>
2023-01-17 23:46:51 +0900

    1463-1로만들기

---
+++ 알고리즘기초1/다이나믹프로그래밍1/1463-1로만들기.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "../ex.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+const num = Number(input);
+
+const dp = new Array(num + 1).fill(0);
+
+for (let i = 2; i <= num; i++) {
+  dp[i] = dp[i - 1] + 1;
+
+  if (i % 2 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+  }
+  if (i % 3 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  }
+}
+
+console.log(dp[num]);
