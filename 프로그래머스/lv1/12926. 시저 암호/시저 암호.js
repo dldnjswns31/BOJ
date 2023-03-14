eb637e9641ab6091d8e93df522ca03532665c7d8
WonJun <71388830+dldnjswns31@users.noreply.github.com>
2023-03-14 21:25:50 +0900

    [level 1] Title: 시저 암호, Time: 1.92 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,9 +1,17 @@
 function solution(s, n) {
+    // 소문자 대문자 문자열
     let lower = 'abcdefghijklmnopqrstuvwxyz';
     let upper = lower.toUpperCase();
     
+    // 주어진 문자를 배열로 변환
+    // ABC
+    // [A,B,C]
     s = s.split('');
+    
+    // 정답 문자열을 담을 배열
     let result = [];
+    
+    // 배열 순회
     for(let item of s) {
         let length = 25;
         if(lower.includes(item)) {
