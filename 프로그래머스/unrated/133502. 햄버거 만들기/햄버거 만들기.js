ae633aef9021c1db2f48e83e11b1c7c2399a9df7
WonJun <71388830+dldnjswns31@users.noreply.github.com>
2023-03-28 21:29:06 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -2,12 +2,15 @@ function solution(ingredient) {
     let answer = 0;
     const stack = [];
     
+    // 아이템 스택에 저장
     for(let item of ingredient) {
         stack.push(item);
         
+        // 스택의 길이가 4 이상일경우 마지막 4개 아이템 확인
         if(stack.length >= 4) {
             let str = stack.slice(-4).join('');
             
+            // 순서가 맞다면 재료 pop후 answer 증가
             if(str === '1231') {
                 stack.pop();
                 stack.pop();
