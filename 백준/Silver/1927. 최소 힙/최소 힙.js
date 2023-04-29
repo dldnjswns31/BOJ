c99145848f51535c34d4ea8510e145ff43ef8918
WonJun <71388830+dldnjswns31@users.noreply.github.com>
2023-04-29 20:12:20 +0900

    [Silver II] Title: 최소 힙, Time: 280 ms, Memory: 28036 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "../ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = Number(input.shift());
+const arr = [...input.map(Number)];
+
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  size() {
+    return this.heap.length - 1;
+  }
+
+  empty() {
+    if (this.size() === 0) return true;
+    else return false;
+  }
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    let curIdx = this.heap.length - 1;
+    let parIdx = (curIdx / 2) >> 0;
+
+    while (curIdx > 1 && this.heap[parIdx] > this.heap[curIdx]) {
+      this.swap(parIdx, curIdx);
+      curIdx = parIdx;
+      parIdx = (curIdx / 2) >> 0;
+    }
+  }
+
+  pop() {
+    const min = this.heap[1];
+    if (this.heap.length <= 2) this.heap = [null];
+    else this.heap[1] = this.heap.pop();
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+
+    if (!this.heap[leftIdx]) return min;
+    if (!this.heap[rightIdx]) {
+      if (this.heap[leftIdx] < this.heap[curIdx]) {
+        this.swap(leftIdx, curIdx);
+      }
+      return min;
+    }
+
+    while (
+      this.heap[leftIdx] < this.heap[curIdx] ||
+      this.heap[rightIdx] < this.heap[curIdx]
+    ) {
+      const minIdx =
+        this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
+      this.swap(minIdx, curIdx);
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+    }
+
+    return min;
+  }
+}
+
+const minHeap = new MinHeap();
+const answer = [];
+
+for (let num of arr) {
+  if (num === 0) {
+    if (minHeap.empty()) answer.push(0);
+    else answer.push(minHeap.pop());
+  } else {
+    minHeap.push(num);
+  }
+}
+
+console.log(answer.join("\n"));
